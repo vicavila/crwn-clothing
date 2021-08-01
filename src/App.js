0723da2c87b6ef06cff22717b552ec1cb67005ab
vicavila <victoral.avila@gmail.com>
2021-08-01 16:52:13 -0500

    routing in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+ <div>
+  <h1>HATS PAGE</h1>
+ </div>
+);
 
 function App() {
  return (
   <div>
-   <HomePage />
+   {/* makes it so that it stops with the first coincidence so that exact will not be necessary */}
+   <Switch>
+    {/* exact: so that it doesn't catch all under it like /hats, alone is equivalent to exact="true", for example if set to false or not included it will render homepage as well as the next (HatsPage) one after the other  */}
+    <Route exact path="/" component={HomePage} />
+    <Route path="/hats" component={HatsPage} />
+   </Switch>
   </div>
  );
 }
