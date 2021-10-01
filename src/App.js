d631a136aa59dbd75ba4b8cc6faccaf8025e7235
vicavila <victoral.avila@gmail.com>
2021-10-01 15:57:09 -0500

    header component and new font

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 function App() {
  return (
   <div>
+   <Header />
    {/* makes it so that it stops with the first coincidence so that exact will not be necessary */}
    <Switch>
     {/* exact: so that it doesn't catch all under it like /hats, alone is equivalent to exact="true", for example if set to false or not included it will render homepage as well as the next (HatsPage) one after the other  */}
