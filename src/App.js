ccc5a1f28a03feab2ae6224860da95378632a734
vicavila <victoral.avila@gmail.com>
2021-10-04 09:02:29 -0500

    sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component';
 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 function App() {
  return (
   <div>
@@ -15,6 +16,7 @@ function App() {
     {/* one caveat with route is that we only can access history, match, etc, from the first component that gets passed into our route, in this case HomePage, we could pass those as props down to the components needed through props but we'll end up with prop drilling where some intermediate components which don't need the history will have to include it just to pass it down, to solve it we can implement withRouter only on the component in which it's needed */}
     <Route exact path="/" component={HomePage} />
     <Route path="/shop" component={ShopPage} />
+    <Route path="/signin" component={SignInAndSignUpPage} />
    </Switch>
   </div>
  );
