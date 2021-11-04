f00462b285914f633fe22fb77377701385f509a9
vicavila <victoral.avila@gmail.com>
2021-11-04 14:00:44 -0500

    create and sign in with email and password

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends React.Component {
        ...snapshot.data(),
       },
      });
+     console.log(this.state);
     });
    } else {
     this.setState({ currentUser: userAuth });
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInGoogle } from '../../firebase/firebase.utils';
+import { auth, signInGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
  constructor(props) {
@@ -12,8 +12,17 @@ class SignIn extends React.Component {
    password: '',
   };
  }
- handleSubmit = (event) => {
+ handleSubmit = async (event) => {
   event.preventDefault();
+
+  const { email, password } = this.state;
+  try {
+   await auth.signInWithEmailAndPassword(email, password);
+   this.setState({ email: '', password: '' });
+  } catch (error) {
+   console.log(error);
+  }
+
   this.setState({ email: '', password: '' });
  };
  handleChange = (event) => {
