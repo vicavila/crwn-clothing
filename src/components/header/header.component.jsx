33750cd7fde22169c5c0310926e6855ff8791e8f
vicavila <victoral.avila@gmail.com>
2021-11-30 19:57:43 -0500

    toggle to hide cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
  <div className="header">
   <Link className="logo-container" to="/">
    <Logo className="logo" to="/" />
@@ -30,12 +30,13 @@ const Header = ({ currentUser }) => (
    )}
    <CartIcon />
   </div>
-  <CartDropdown></CartDropdown>
+  {hidden ? null : <CartDropdown />}
  </div>
 );
 
-const mapStateToProps = (state) => ({
- currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+ currentUser,
+ hidden,
 });
 
 export default connect(mapStateToProps)(Header);
