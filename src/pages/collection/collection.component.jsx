8c6877f797893d950322b94595616a8462f8e4ae
vicavila <victoral.avila@gmail.com>
2021-12-14 06:45:51 -0500

    finish collection and link from directory

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,10 +8,16 @@ import './collection.styles.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 const CollectionPage = ({ collection }) => {
+ const { title, items } = collection;
  console.log(collection);
  return (
   <div className="collection-page">
-   <h2>COLLECTION PAGE</h2>
+   <h2 className="title">{title}</h2>
+   <div className="items">
+    {items.map((item) => (
+     <CollectionItem key={item.id} item={item} />
+    ))}
+   </div>
   </div>
  );
 };
