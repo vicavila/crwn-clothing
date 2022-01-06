9205dc2d8bf5fd0e4bdb55a714f19a53d6de6264
vicavila <victoral.avila@gmail.com>
2022-01-06 06:43:16 -0500

    remove shop data file and add spinner when loading from backend

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,23 +9,43 @@ import {
 import { collection, onSnapshot } from 'firebase/firestore';
 import { connect } from 'react-redux';
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+ state = { loading: true };
  unsubscripbeFromSnapshot = null;
  async componentDidMount() {
   const { updateCollections } = this.props;
   const collectionRef = await collection(firestore, 'collections');
-  onSnapshot(collectionRef, async (snapshot) => {
-   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-   updateCollections(collectionsMap);
-  });
+  this.unsubscripbeFromSnapshot = onSnapshot(
+   collectionRef,
+   async (snapshot) => {
+    const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    updateCollections(collectionsMap);
+    this.setState({ loading: false });
+   }
+  );
  }
  render() {
   const { match } = this.props;
+  const { loading } = this.state;
   return (
    <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    <Route
+     exact
+     path={`${match.path}`}
+     render={(props) => (
+      <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+     )}
+    />
+    <Route
+     path={`${match.path}/:collectionId`}
+     render={(props) => (
+      <CollectionPageWithSpinner isLoading={loading} {...props} />
+     )}
+    />
    </div>
   );
  }
