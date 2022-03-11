e31e405b063fc2fb687076fc6c586a8766c97464
vicavila <victoral.avila@gmail.com>
2022-03-11 07:10:24 -0500

    implement generator function with saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,8 +1,28 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import shopActionTypes from './shop.types';
+import {
+ firestore,
+ convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import {
+ fetchCollectionsSuccess,
+ fetchCollectionsFailure,
+} from './shop.actions';
+import { collection, getDocs } from 'firebase/firestore';
 
 export function* fetchCollectionsAsync() {
- yield console.log('I am fired');
+ try {
+  const collectionRef = collection(firestore, 'collections');
+  const snapshot = yield getDocs(collectionRef);
+  // the call method is a saga effect which defer control on the function execution
+  // back to the saga middleware, we use it whenever there's a potential slow function,
+  // it receives as parameters the function and then the parameters
+  const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+  // put is the saga effect for creating actions, like a dispatch in thunks
+  yield put(fetchCollectionsSuccess(collectionsMap));
+ } catch (error) {
+  yield put(fetchCollectionsFailure(error.message));
+ }
 }
 
 export function* fetchCollectionsStart() {
